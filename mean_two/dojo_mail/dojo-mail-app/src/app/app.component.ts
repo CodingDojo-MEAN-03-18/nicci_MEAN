7c9e83efe4c9fcc2fc8155541471f0ffb8e4b15e
Nicci Lee Abueg <nicci.abueg@gmail.com>
2018-05-10 01:12:01 -0600

    trivia

--- mean_two/dojo_mail/dojo-mail-app/src/app/app.component.ts
+++ mean_two/dojo_mail/dojo-mail-app/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  emails = [
+    {email: "blank@blank.com", importance: false, subject: 'unknown', content: 'unknown content'},
+    {email: "maya@blank.com", importance: true, subject: 'maya', content: 'maya is awesome' },
+    {email: "boys@blank.com", importance: false, subject: 'ash and ollie', content: 'little boys are awesome' },
+  ]
+  title = 'dojo mail';
 }
+// var arrayOfEmails: Array<string>;
+// var subject: string = "subject string";
+// var content: string = "content string";
+// var importance: boolean = true;
+
+// email_list = obj {
+
+// }
+
